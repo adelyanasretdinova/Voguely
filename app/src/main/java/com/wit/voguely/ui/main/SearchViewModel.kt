29b4aea642edf11792960a4e113014dd1b8dd997
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2023-01-26 13:52:08 +0100

    search function works

--- app/src/main/java/com/wit/voguely/ui/main/SearchViewModel.kt
+++ app/src/main/java/com/wit/voguely/ui/main/SearchViewModel.kt
@@ -2,28 +2,31 @@ package com.wit.voguely.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.wit.voguely.remote.ProductsDataSource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class SearchViewModel : ViewModel() {
-
+    private val productsDataSource = ProductsDataSource()
     private val _searchedItems = MutableStateFlow<List<Products>>(listOf())
     val searchedItems = _searchedItems.asStateFlow()
     private val _noResult = MutableStateFlow<Boolean>(false)
     val noResult = _noResult.asStateFlow()
 
+//    init {
+//        dataLoad()
+//    }
 
     fun search(s: CharSequence) {
 
         viewModelScope.launch {
 
             _searchedItems.update {
-                listOf<Products>().filter { it.name.contains(s, ignoreCase = true) }
+                productsDataSource.getProducts().filter { it.name.contains(s, ignoreCase = true) }
             }
-                _noResult.update { _searchedItems.value.isEmpty()}
+            _noResult.update { _searchedItems.value.isEmpty() }
         }
     }
 }
